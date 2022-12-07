93a192c41ee4066e582903f2e059dd2ce4d26c76
salamoposoDev <salamoposo.dev@gmail.com>
2022-12-08 03:13:52 +0800

    tambahan ui untuk tombol buat

--- lib/pages/person.dart
+++ lib/pages/person.dart
@@ -36,15 +36,165 @@ class PersonPage extends StatelessWidget {
               Row(
                 // ignore: prefer_const_literals_to_create_immutables
                 children: [
+                  //ADD POST BUTTON
                   GestureDetector(
                     onTap: () {
                       showModalBottomSheet(
+                          backgroundColor: Colors.transparent,
                           context: context,
                           builder: (BuildContext context) {
                             return Container(
-                              child: Text('Bottom Sheet'),
+                              child: Column(
+                                children: [
+                                  SizedBox(height: 10),
+                                  Text(
+                                    'Buat',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  SizedBox(height: 30),
+                                  // rells
+                                  Container(
+                                    child: Row(
+                                      children: [
+                                        SizedBox(width: 10),
+                                        Icon(
+                                          Icons.video_call,
+                                          size: 30,
+                                        ),
+                                        SizedBox(width: 10),
+                                        Text(
+                                          'Reel',
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                      ],
+                                    ),
+                                    constraints: BoxConstraints.tightForFinite(
+                                      width: 400,
+                                      height: 50,
+                                    ),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+
+                                  // Postingan
+                                  Container(
+                                    child: Row(
+                                      children: [
+                                        SizedBox(width: 10),
+                                        Icon(
+                                          Icons.grid_4x4_outlined,
+                                          size: 30,
+                                        ),
+                                        SizedBox(width: 10),
+                                        Text(
+                                          'Postingan',
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                      ],
+                                    ),
+                                    constraints: BoxConstraints.tightForFinite(
+                                      width: 400,
+                                      height: 50,
+                                    ),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ),
+
+                                  // SOROTAN CERITA
+                                  Container(
+                                    child: Row(
+                                      children: [
+                                        SizedBox(width: 10),
+                                        Icon(
+                                          Icons.add_a_photo_outlined,
+                                          size: 30,
+                                        ),
+                                        SizedBox(width: 10),
+                                        Text(
+                                          'Sorotan Cerita',
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                      ],
+                                    ),
+                                    constraints: BoxConstraints.tightForFinite(
+                                      width: 400,
+                                      height: 50,
+                                    ),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+
+                                  // SIARAN LANGSUNG
+                                  Container(
+                                    child: Row(
+                                      children: [
+                                        SizedBox(width: 10),
+                                        Icon(
+                                          Icons.live_tv_outlined,
+                                          size: 30,
+                                        ),
+                                        SizedBox(width: 10),
+                                        Text(
+                                          'Siaran Langsung',
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                      ],
+                                    ),
+                                    constraints: BoxConstraints.tightForFinite(
+                                      width: 400,
+                                      height: 50,
+                                    ),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ),
+
+                                  // PANDUAN
+
+                                  Container(
+                                    child: Row(
+                                      children: [
+                                        SizedBox(width: 10),
+                                        Icon(
+                                          Icons.bookmarks_outlined,
+                                          size: 30,
+                                        ),
+                                        SizedBox(width: 10),
+                                        Text(
+                                          'Panduna',
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                      ],
+                                    ),
+                                    constraints: BoxConstraints.tightForFinite(
+                                      width: 400,
+                                      height: 50,
+                                    ),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                               height: 400,
                               decoration: BoxDecoration(
+                                color: Colors.grey[100],
                                 borderRadius: BorderRadius.circular(10),
                               ),
                             );
@@ -220,8 +370,9 @@ class PersonPage extends StatelessWidget {
                         height: 30,
                         width: 170,
                         decoration: BoxDecoration(
-                            color: Colors.grey[300],
-                            borderRadius: BorderRadius.circular(10)),
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
                       Container(
                         // ignore: sort_child_properties_last
